73c04e7a408267d908d28f1e50a6b2ae174dec87
uddhav29 <uddhav29@users.noreply.github.com>
2018-09-04 15:53:46 +0530

    update message

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -17,7 +17,7 @@ public class UserController {
 	public String getUserName(@RequestParam(name="name")String name) {
 		
 		//implementation of project
-		return "Welcome to P5 (Code Athon), "+name;
+		return "Welcome to CodeAthon, "+name;
 	}
 	
 	@RequestMapping(method=RequestMethod.POST ,value="welcome/post")
